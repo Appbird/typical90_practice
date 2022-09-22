8d250dc274a6d319028b9605abb150bd55588984
Appbird <project.appbird@gmail.com>
2022-09-22 10:11:52 +0900

    place cargo.toml and something again.

---
+++ src/q4.rs
@@ -0,0 +1,15 @@
+fn cin() -> i32{
+    let loaded:i32;
+    io::stdin()
+        .read_line(&mut loaded)
+        .expect("Failed to readline");
+    return match loaded.trim().parse().expect("Failed to convert to i32");
+    
+}
+fn main() {
+    let mut h:i32;
+    let mut w:i32;
+    io::stdin()
+        
+    let mut A:i32[][]:
+}
