df71455ba252a2251313dcf1a4978a88d06663d5
Appbird <project.appbird@gmail.com>
2022-09-22 10:56:08 +0900

    complete environment construction of Rust for the competitive programming.

--- src/bin/q4.rs
+++ src/bin/q4.rs
@@ -1,15 +1,15 @@
+use std::io;
+
+
 fn cin() -> i32{
-    let loaded:i32;
+    let mut loaded:String = String::new();
     io::stdin()
         .read_line(&mut loaded)
         .expect("Failed to readline");
-    return match loaded.trim().parse().expect("Failed to convert to i32");
+    return loaded.trim().parse().expect("Failed to convert to i32");
     
 }
 fn main() {
     let mut h:i32;
     let mut w:i32;
-    io::stdin()
-        
-    let mut A:i32[][]:
 }
